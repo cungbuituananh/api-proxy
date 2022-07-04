82e62a2aa1ed61db6f7cb6ab17b200dfe96cb653
Duc Dao Anh <duc.dao@ascendcorp.com>
2022-07-04 21:15:32 +0700

    Fix bug maxServiceExtensions

--- middlewares/custom.js
+++ middlewares/custom.js
@@ -2,18 +2,19 @@ const {callRequest} = require('../helpers/api');
 const {config} = require('../config');
 
 exports.createMaxServiceExtensions = async (req, res, next) => {
+  next();
 
   if (res.response && res.response.data && res.response.data.id) {
 
-    const createMaxServiceExtensions = await callRequest(req, "POST",
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    await callRequest(req, "POST",
         `/rest/orgUnitAttributes`, {}, {
           name: "maxServiceExtensions",
           value: config.maxServiceExtensions,
           orgUnitId: res.response.data.id
         });
 
-    console.log('createMaxServiceExtensions', createMaxServiceExtensions);
-    next();
   } else {
     next();
   }
